4ca0a409d2b5c03c7460a10795717d288025cc77
Erin De Pree <erindepree@gmail.com>
2025-08-29 00:48:16 +0000

    basic model, predicts all job ads are frauds!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import pickle
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Is this Job ad a fake?")
+#st.write(
+#    "text"
+#)
+job_ad = st.text_input("Copy and paste the job ad here:", key='job_ad_text')
+
+#with open('model_tfidf.pickle', 'rb') as f:
+#    model = pickle.load(f)
+
+if st.button('check if fraud'):
+    pred = 1 #model.predict(job_ad)
+    if pred == 1:
+        st.write("The model predicts this job ad is fraudulent!")
+    else:
+        st.write("The model predicts this job ad is legit!")
+
+st.link_button('To read about this model, please visit my GitHub', "https://github.com/erindepree/fake_job_ads")
